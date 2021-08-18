8cc990f0af6d5f654a32b487221042a6a0f3e052
bishalr0y <bishalroy895@gmail.com>
2021-08-18 09:30:04 +0530

    occurrence program added

---
+++ conditionals-and-loops/src/com/bishal/Occurrence.java
@@ -0,0 +1,23 @@
+package com.bishal;
+import java.util.Scanner;
+
+//To find the occurrence of a number
+
+public class Occurrence {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number to check for the occurrence: ");
+        int num = input.nextInt();
+        System.out.println("Enter the number whose occurrence you want to check: ");
+        int n = input.nextInt();
+        int rem, count = 0;
+        while (num%10 != 0) {
+            rem = num % 10;
+            if (rem == n) {
+                count++;
+            }
+            num /= 10;
+        }
+        System.out.println("The occurrence of the number 7 is: " + count);
+    }
+}
