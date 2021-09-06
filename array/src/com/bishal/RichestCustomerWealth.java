3055f3ac24e4637097658383d3a994adb92d387f
bishalr0y <bishalroy895@gmail.com>
2021-09-06 17:57:07 +0530

    Richest Customer Wealth programme modified

--- array/src/com/bishal/RichestCustomerWealth.java
+++ array/src/com/bishal/RichestCustomerWealth.java
@@ -25,6 +25,7 @@ public class RichestCustomerWealth {
      static void getRichestCustomerWealth(int[][] arr) {
         int max = 0, sum = 0;
          for (int i = 0; i <arr.length ; i++) {
+             sum = 0;
              for (int j = 0; j <arr[i].length ; j++) {
                  sum += arr[i][j];
              }
